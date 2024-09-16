2d3e405def4acbfb7847290f37326ab4d59679d9
vietho <vietho@ww-system.com>
2024-09-16 23:35:06 +0700

    init project

---
+++ database/migrations/2024_09_16_160444_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('competition_id');
+            $table->string('country_id');
+            $table->string('name');
+            $table->string('logo');
+            $table->timestamps();
+            
+            $table->foreign('competition_id')->references('id')->on('competitions');
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/migrations/2024_09_16_161050_create_matches_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('competition_id');
+            $table->string('home_team_id');
+            $table->string('away_team_id');
+            $table->tinyInteger('status_id');
+            $table->unsignedInteger('match_time');
+            $table->json('home_scores');
+            $table->json('away_scores');
+            $table->timestamps();
+
+            $table->foreign('competition_id')->references('id')->on('competitions');
+            $table->foreign('home_team_id')->references('id')->on('teams');
+            $table->foreign('away_team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
